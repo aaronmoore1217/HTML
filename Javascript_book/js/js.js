0c7963f2c6874922502e8b6ff8049d846c30c6d3
aaronmoore1217 <126786373+aaronmoore1217@users.noreply.github.com>
2023-04-20 10:11:06 -0400

    .

--- Javascript_book/js/js.js
+++ Javascript_book/js/js.js
@@ -18,31 +18,44 @@
 // firstItem.className = 'complete';
 // lastItem.className = 'cool';
 
-let firstItem = document.getElementById('one');
-let TextContent = firstItem.textContent;
-let InnerText = firstItem.innerText;
+// let firstItem = document.getElementById('one');
+// let TextContent = firstItem.textContent;
+// let InnerText = firstItem.innerText;
 
-let msg = '<p>textContent: ' + TextContent + '</p>';
-msg += '<p> innerText: ' + InnerText + '</p>';
+// let msg = '<p>textContent: ' + TextContent + '</p>';
+// msg += '<p> innerText: ' + InnerText + '</p>';
+
+// let el = document.getElementById('scriptResults');
+// el.innerHTML = msg;
+
+// // firstItem.textContent = 'Sourdough Bread';
+// firstItem.innerHTML = '<a href=\"http://example.org\">' + firstItem.textContent + '</a>';
+
+// let newEl = document.createElement('li');
+// let newText = document.createTextNode('https');
+
+// //first, attach the text node to the elements
+// newEl.appendChild(newText);
+// //find/decide where to put the new element
+// let position = document.getElementsByTagName('ul')[0];
+// position.appendChild(newEl);
+
+// let remEl = document.getElementsByTagName('li')[3];
+
+// //to remove, first get parent node
+// let parentEl = remEl.parentNode;
+// //then remove
+// parentEl.removeChild(remEl);
+
+let firstItem = document.getElementById('one');
+
+if (firstItem.hasAttribute('class')){
+    // let attr = firstItem.getAttribute('class');
+    firstItem.className = 'complete';
+    
+}
+let fourthItem = document.getElementsByTagName('li')[3];
+fourthItem.setAttribute('class', 'cool');
 
 let el = document.getElementById('scriptResults');
-el.innerHTML = msg;
-
-// firstItem.textContent = 'Sourdough Bread';
-firstItem.innerHTML = '<a href=\"http://example.org\">' + InnerHTML + '</a>';
-
-let newEl = document.createElement('li');
-let newText = document.createTextNode('https');
-
-//first, attach the text node to the elements
-newEl.appendChild(newText);
-//find/decide where to put the new element
-let position = document.getElementsByTagName('ul')[0];
-position.appendChild(newEl);
-
-let remEl = document.getElementsByTagName('li')[3];
-
-//to remove, first get parent node
-let parentEl = remEl.parentNode;
-//then remove
-parentEl.removeChild(remEl);
+el.innerHTML = '<p>The first item has a class name: ' + attr + '</p>'
