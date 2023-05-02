492792835b78b0d56ee446381fa9dd633a878dbb
aaronmoore1217 <126786373+aaronmoore1217@users.noreply.github.com>
2023-05-02 10:34:38 -0400

    .

--- JSON/js/data-json.js
+++ JSON/js/data-json.js
@@ -9,12 +9,19 @@ xhr.onload = function() {
         let newContent = '';
 
         //loop through object to obtain all elements/data to add to the webpage
-        for( let i = 0; i < 1; ++i){
-            newContent += '';
+        for( let i = 0; i < responseObject.events.length; ++i){
+            newContent += '<div class="event">';
+            newContent += '<img src="' + responseObject.events[i].map + '" ';
+            newContent += 'alt="' + responseObject.events[i].location + '" />'
+            newContent += '<p><b>' + responseObject.events[i].location + '<br>'
+            newContent += responseObject.events[i].date + '</b></p>'
+            newContent += '</div>';
         }
+
+        document.getElementById('content').innerHTML = newContent;
     }
 }
 
-xhr.open(); //router.open
+xhr.open('GET', 'data/data.json', true); //router.open
 
 xhr.send(null); //router.recieve
