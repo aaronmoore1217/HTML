2400087358065ad1cfb1e9176017439e3b56dc34
aaronmoore1217 <126786373+aaronmoore1217@users.noreply.github.com>
2023-05-18 14:14:46 -0400

    Co-authored-by: ps24KB <ps24KB@users.noreply.github.com>

--- JSON Test/jsonReader.js
+++ JSON Test/jsonReader.js
@@ -6,11 +6,18 @@ function createSection(element){
 }
 function createEl(element, text){
     //creates a new li element, adds a text node to it, and returns
-    let newLi = document.createElement(element);
-    let textNode = document.createTextNode(text);
-    newLi.appendChild(textNode);
-
-    return newLi;
+    if (element === "img"){
+        let newLi = document.createElement(element);
+        newLi.src = text;
+        return newLi;
+    }
+    else {
+        let newLi = document.createElement("div");
+        newLi.className = element;
+        let textNode = document.createTextNode(text);
+        newLi.appendChild(textNode);
+        return newLi;
+    }
 }
 
 
@@ -23,14 +30,26 @@ xhr.onload = function() {
     if(xhr.status === 200){
         let responseObject = JSON.parse(xhr.responseText);
         let content = document.querySelector("body");
-        let Jason = responseObject.content;
+        let Jason = responseObject.Restaurants;
 
         //loop through object to obtain all elements/data to add to the webpage
-        for( let i = 0; i < Jason.length; ++i){
-            let box = createSection(Jason[i].container)
-            box.appendChild(createEl(Jason[i].header.element, Jason[i].header.text))
-            box.appendChild(createEl(Jason[i].body.element, Jason[i].body.text))
-            content.appendChild(box)
+        for(let i = 0; i < Jason.length; ++i){
+            let container = createSection(Jason[i].container);
+            let imgBox = createSection(Jason[i].imgContainer);
+            let textBox = createSection(Jason[i].textContainer)
+            
+
+            for (let b = 0; b < Jason[i].content.length; ++b){
+                if (Jason[i].content[b].element === "img"){
+                    imgBox.appendChild(createEl(Jason[i].content[b].element, Jason[i].content[b].text))
+                }
+                else {
+                    textBox.appendChild(createEl(Jason[i].content[b].element, Jason[i].content[b].text))
+                }
+            }
+            container.appendChild(imgBox);
+            container.appendChild(textBox);
+            content.appendChild(container);
         }
     }
 }
